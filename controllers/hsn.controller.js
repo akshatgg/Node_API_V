e9262d9b48b2a69afccdb0d1cdf035112080913d
gaurav suman <gaurav.quarkits@gmail.com>
2022-11-19 23:49:42 +0530

    get all

--- controllers/hsn.controller.js
+++ controllers/hsn.controller.js
@@ -64,6 +64,62 @@ class HsnController {
     });
  }
 
+ getHsnAll = async (req, res, next) => {
+   
+    Hsn.findAll({
+        
+
+        
+            //where: { id: req.body.id }
+        
+    }).then((result) => {
+      
+        if (result) {
+            return res.status(200).json({
+                status: true,
+                message: "hsn code",
+                data: result
+            })
+        } else {
+            return next(ApiError.badRequest("failed to get hsn code"))
+        }
+    }).catch((error) => {
+        console.log(`catch block ${error}`)
+        if (error)
+            return next(ApiError.conflict(error));
+        else
+            return next(ApiError.internalServerError(error))
+    });
+ }
+
+ getSacAll = async (req, res, next) => {
+    
+    Sac.findAll({
+        
+
+        
+           // where: { id: req.body.id }
+        
+    }).then((result) => {
+       
+        if (result) {
+            return res.status(200).json({
+                status: true,
+                message: "Sac code",
+                data: result
+            })
+        } else {
+            return next(ApiError.badRequest("failed to get hsn code"))
+        }
+    }).catch((error) => {
+        console.log(`catch block ${error}`)
+        if (error)
+            return next(ApiError.conflict(error));
+        else
+            return next(ApiError.internalServerError(error))
+    });
+ }
+
 }
 
 
--- routes/hsn.js
+++ routes/hsn.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const hsnController = require('../controllers/hsn.controller')
 router.get('/getHsn', hsnController.getHsnCode);
 router.get('/getSac', hsnController.getSacCode);
+router.get('/getHsnAll', hsnController.getHsnAll);
+router.get('/getSacAll', hsnController.getSacAll);
 module.exports = router;
