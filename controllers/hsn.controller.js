02c16948d1b84611aef40823aec086ec3ce76b78
gaurav suman <gaurav.quarkits@gmail.com>
2022-11-19 22:21:15 +0530

    hsn and sac code

--- controllers/hsn.controller.js
+++ controllers/hsn.controller.js
@@ -1,4 +1,5 @@
 const { Hsn } = require('../models');
+const { Sac } = require('../models');
 const uuid = require('uuid');
 const bCrypt = require('bcrypt-nodejs');
 const jwt = require('jsonwebtoken');
@@ -8,26 +9,54 @@ const ApiError = require('../errors/ApiError');
 
 class HsnController {
   getHsnCode = async (req, res, next) => {
-    var token = req.header('authorization')
-    var payload = decodeToken(token)
+    //var token = req.header('authorization')
+   // var payload = decodeToken(token)
+   console.log(req.query.id);
+   console.log(req.body.id);
     Hsn.findOne({
-        include: [{
-            model: Hsn,
-            where: { id: payload.id }
-        }]
+        
+
+        
+            where: { id: req.body.id }
+        
     }).then((result) => {
-        console.log(result["dataValues"].User["dataValues"]);
+       // console.log(result["dataValues"].User["dataValues"]);
         if (result) {
             return res.status(200).json({
                 status: true,
                 message: "hsn code",
-                data: {
-                    id: result['dataValues']['id'],
-                    hsn_code: result['dataValues']['hsn_code'],
-                    description: result['dataValues']['description'],
-                   
+                data: result
+            })
+        } else {
+            return next(ApiError.badRequest("failed to get hsn code"))
+        }
+    }).catch((error) => {
+        console.log(`catch block ${error}`)
+        if (error)
+            return next(ApiError.conflict(error));
+        else
+            return next(ApiError.internalServerError(error))
+    });
+ }
 
-                }
+ getSacCode = async (req, res, next) => {
+    //var token = req.header('authorization')
+   // var payload = decodeToken(token)
+   console.log(req.query.id);
+   console.log(req.body.id);
+    Sac.findOne({
+        
+
+        
+            where: { id: req.body.id }
+        
+    }).then((result) => {
+       // console.log(result["dataValues"].User["dataValues"]);
+        if (result) {
+            return res.status(200).json({
+                status: true,
+                message: "Sac code",
+                data: result
             })
         } else {
             return next(ApiError.badRequest("failed to get hsn code"))
--- routes/hsn.js
+++ routes/hsn.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const hsnController = require('../controllers/hsn.controller')
 router.get('/getHsn', hsnController.getHsnCode);
+router.get('/getSac', hsnController.getSacCode);
 module.exports = router;
