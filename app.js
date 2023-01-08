21b0875eba57a51493b31e241952bf1fd3ead3d2
gaurav suman <gaurav.quarkits@gmail.com>
2023-01-08 13:09:57 +0530

    access control

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cors(corsOpts));
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 }
 app.use(allowCrossDomain);
