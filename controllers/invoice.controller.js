53556803b993d9207e0df6e9b35a07670b91e369
Ashu <a851601@gmail.com>
2023-01-19 13:11:21 +0530

    invoice api

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -1,103 +1,235 @@
 // const { Invoice, UserProfile, UserBusinessProfile } = require('../models');
 // const ApiError = require('../errors/ApiError');
+
 // class InvoiceController {
 //     createInvoice = async (req, res, next) => {
-//         var token = req.header('authorization')
+//         var token = req.header('authorization');
 //         if (token) {
-//             var payload = decodeToken(token)
+//             var payload = decodeToken(token);
 //             // console.log("token ", token);
 //             var invoice = new Invoice(req.body);
 //             invoice.user_id = payload.id;
-//             invoice.save().then((invoice) => {
-//                 res.status(200).json({
-//                     status: "success",
-//                     message: "invoice created successfully",
-//                     data: invoice
+//             invoice
+//                 .save()
+//                 .then((invoice) => {
+//                     res.status(200).json({
+//                         status: 'success',
+//                         message: 'invoice created successfully',
+//                         data: invoice,
+//                     });
+//                 })
+//                 .catch((error) => {
+//                     res.status(500).json({
+//                         status: 'error',
+//                         message: 'invoice not created',
+//                         error: error,
+//                     });
 //                 });
-//             }
-//             ).catch((error) => {
-//                 res.status(500).json({
-//                     status: "error",
-//                     message: "invoice not created",
-//                     error: error
-//                 });
-//             }
-//             );
 //         } else {
 //             res.status(403).json({
-//                 status: "error",
-//                 message: "token not found"
+//                 status: 'error',
+//                 message: 'token not found',
 //             });
 //         }
-//     }
+//     };
+
 //     getInvoice = async (req, res, next) => {
-//         var token = req.header('authorization')
+//         var token = req.header('authorization');
 //         if (token) {
-//             var payload = decodeToken(token)
+//             var payload = decodeToken(token);
 //             Invoice.findAll({
 //                 where: {
-//                     user_id: payload.id
-//                 }
-//             }).then((invoice) => {
-//                 res.status(200).json({
-//                     status: "success",
-//                     message: "invoice fetched successfully",
-//                     data: invoice
+//                     user_id: payload.id,
+//                 },
+//             })
+//                 .then((invoice) => {
+//                     res.status(200).json({
+//                         status: 'success',
+//                         message: 'invoice fetched successfully',
+//                         data: invoice,
+//                     });
+//                 })
+//                 .catch((error) => {
+//                     res.status(500).json({
+//                         status: 'error',
+//                         message: 'invoice not fetched',
+//                         error: error,
+//                     });
 //                 });
-//             }
-//             ).catch((error) => {
-//                 res.status(500).json({
-//                     status: "error",
-//                     message: "invoice not fetched",
-//                     error: error
-//                 });
-//             }
-//             );
 //         } else {
 //             res.status(403).json({
-//                 status: "error",
-//                 message: "token not found"
+//                 status: 'error',
+//                 message: 'token not found',
 //             });
 //         }
-//     }
+//     };
+
 //     updateInvoice = async (req, res, next) => {
-//         var token = req.header('authorization')
+//         var token = req.header('authorization');
 //         if (token) {
-//             var payload = decodeToken(token)
+//             var payload = decodeToken(token);
 //             Invoice.update(req.body, {
 //                 where: {
-//                     id: req.body.id
-//                 }
-//             }).then((invoice) => {
-//                 res.status(200).json({
-//                     status: "success",
-//                     message: "invoice updated successfully",
-//                     // data: invoice
+//                     id: req.body.id,
+//                 },
+//             })
+//                 .then((invoice) => {
+//                     res.status(200).json({
+//                         status: 'success',
+//                         message: 'invoice updated successfully',
+//                         // data: invoice
+//                     });
+//                 })
+//                 .catch((error) => {
+//                     res.status(500).json({
+//                         status: 'error',
+//                         message: 'invoice not updated',
+//                         error: error,
+//                     });
 //                 });
-//             }
-//             ).catch((error) => {
-//                 res.status(500).json({
-//                     status: "error",
-//                     message: "invoice not updated",
-//                     error: error
-//                 });
-//             }
-//             );
 //         } else {
 //             res.status(403).json({
-//                 status: "error",
-//                 message: "token not found"
+//                 status: 'error',
+//                 message: 'token not found',
 //             });
 //         }
-//     }
+//     };
 // }
+const { Party, Item } = require('../models/invoice');
 
-// decodeToken = (token) => {
-//     var base64Url = token.split('.')[1];
-//     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//     const buff = new Buffer(base64, 'base64');
-//     const payloadinit = buff.toString('ascii');
-//     const payload = JSON.parse(payloadinit);
-//     return payload;
-// }
+decodeToken = (token) => {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const buff = new Buffer(base64, 'base64');
+    const payloadinit = buff.toString('ascii');
+    const payload = JSON.parse(payloadinit);
+    return payload;
+};
+
+exports.getParties = async (req, res) => {
+    var token = req.header('authorization');
+    if (token) {
+        var payload = decodeToken(token);
+        try {
+            const data = await Party.findAll({
+                where: {
+                    id: payload.id,
+                    type: req.query.type,
+                },
+                offset: req.body.pageNo ? req.body.pageNo * LIMIT : 0,
+                limit: LIMIT,
+                order: [['name', 'DESC']],
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                error: error,
+            });
+        }
+    } else {
+        res.status(403).json({
+            status: 'error',
+            message: 'token not found',
+        });
+    }
+};
+
+exports.getPartyById = async (req, res) => {
+    var token = req.header('authorization');
+    if (token) {
+        var payload = decodeToken(token);
+        try {
+            const data = await Party.findOne({
+                where: {
+                    userId: payload.id,
+                    id: req.params.id,
+                },
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                error: error,
+            });
+        }
+    } else {
+        res.status(403).json({
+            status: 'error',
+            message: 'token not found',
+        });
+    }
+};
+
+exports.getItems = async (req, res) => {
+    var token = req.header('authorization');
+    if (token) {
+        var payload = decodeToken(token);
+        try {
+            const data = await Item.findAll({
+                where: {
+                    userId: payload.id,
+                },
+                offset: req.body.pageNo ? req.body.pageNo * LIMIT : 0,
+                limit: LIMIT,
+                order: [['name', 'DESC']],
+                // offset: req.body.offset ? req.body.offset : 0,
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                error: error,
+            });
+        }
+    } else {
+        res.status(403).json({
+            status: 'error',
+            message: 'token not found',
+        });
+    }
+};
+
+exports.getItemById = async (req, res) => {
+    var token = req.header('authorization');
+    if (token) {
+        var payload = decodeToken(token);
+        try {
+            const data = await Item.findOne({
+                where: {
+                    userId: payload.id,
+                    id: req.params.id,
+                },
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data,
+            });
+        } catch (error) {
+            res.status(500).json({
+                status: 'error',
+                error: error,
+            });
+        }
+    } else {
+        res.status(403).json({
+            status: 'error',
+            message: 'token not found',
+        });
+    }
+};
+
 // module.exports = new InvoiceController();
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,14 +1,31 @@
-const express = require("express");
-const {createInvoice, updateInvoice, deleteInvoice, getInvoice, getInvoicesByUser, getTotalCount } =require('../controllers/invoices.js');
+const express = require('express');
+const {
+    createInvoice,
+    updateInvoice,
+    deleteInvoice,
+    getInvoice,
+    getInvoicesByUser,
+    getTotalCount,
+} = require('../controllers/invoices');
 
-const router = express.Router()
+const {
+    getParties,
+    getPartyById,
+    getItems,
+} = require('../controllers/invoice.controller');
 
-router.get('/count', getTotalCount) //use to generate invoice serial number
-router.get('/:id', getInvoice)
-router.get('/', getInvoicesByUser)
-router.post('/', createInvoice)
-router.patch('/:id', updateInvoice)
-router.delete('/:id', deleteInvoice)
+const router = express.Router();
 
+router.get('/count', getTotalCount); //use to generate invoice serial number
+router.get('/:id', getInvoice);
+router.get('/', getInvoicesByUser);
+router.post('/', createInvoice);
+router.patch('/:id', updateInvoice);
+router.delete('/:id', deleteInvoice);
+
+router.get('/parties', getParties);
+router.get('/party/:id', getPartyById);
+router.get('/items', getItems);
+router.get('/item/:id', getPartyById);
 
 module.exports = router;
