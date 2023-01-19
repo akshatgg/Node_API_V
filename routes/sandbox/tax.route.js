8c7513044ef5a19662569e93b02b47859616b149
prabhat148 <78421243+prabhat148@users.noreply.github.com>
2023-01-19 13:47:53 +0530

    sandbox Ledger api done

--- routes/sandbox/tax.route.js
+++ routes/sandbox/tax.route.js
@@ -7,5 +7,8 @@ const queryValidator = require('../../middleware/query-validator')
 router.get('/get-cash-itc-balence',checkAuth, queryValidator,taxController.cashItcBalance);
 router.get('/get-itc-chash-ledgers',checkAuth, queryValidator,taxController.cashLedger);
 router.get('/get-itc-legers',checkAuth, queryValidator,taxController.itcLedgers);
+router.get('/tax-liability-legers',checkAuth, queryValidator,taxController.tax_liability_ledger);
+router.get('/other-ledgers',checkAuth, queryValidator,taxController.other_Ledger);
+router.get('/return-related-liability-balnce',checkAuth, queryValidator,taxController.return_related_liability_balance);
 
 module.exports = router;
