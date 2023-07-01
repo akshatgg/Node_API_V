9a516c5bd9613780798578ecfff0952f3da24c07
Vishwas-guptaa <vishwasgupta81779@gmail.com>
2023-07-01 13:09:23 +0530

    all profile api

--- src/controllers/profile.contrller.ts
+++ src/controllers/profile.contrller.ts
@@ -3,6 +3,8 @@ import { Profile, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// create profile
+
 class ProfileController {
   static async createProfile(req: Request, res: Response): Promise<void> {
     try {
@@ -21,7 +23,7 @@ class ProfileController {
 
       const  Profile = await prisma.profile.create({
         data: {
-          userId,
+         
           name,
           email,
           phoneNumber,
@@ -41,6 +43,7 @@ class ProfileController {
     }
   }
 
+  // find All profile
   static async findAllProfile(req: Request, res: Response): Promise<void>{
     try{
         const  allprofile = await prisma.profile.findMany({})
@@ -51,7 +54,97 @@ class ProfileController {
 
     }
   }
+
+  //get profile by id
+  
+  static async findOneProfile(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
+  
+      const profile: Profile | null = await prisma.profile.findUnique({
+        where: {
+          Id: id,
+        },
+      });
+  
+      if (!profile) {
+        res.status(404).json({ success: false, message: 'Profile not found' });
+        return;
+      }
+  
+      res.status(200).json(profile);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  }
+//profile update by id
+  static async updateProfile(req: Request, res: Response): Promise<void>{
+    try{
+      const profileId = parseInt(req.params.id);
+      const {
+        userId,
+        name,
+        email,
+        phoneNumber,
+        businessName,
+        contactAddress,
+        gstin,
+        paymentDetails,
+        notes,
+        logo,
+      } = req.body;
+      const updatedProfile: Profile | null = await prisma.profile.update({
+        where: { Id: profileId },
+        data:{
+          name,
+          email,
+          phoneNumber,
+          businessName,
+          contactAddress,
+          gstin,
+          paymentDetails,
+          notes,
+          logo,
+        }
+      })
+      if (!updatedProfile) {
+        res.status(404).json({ sucess: false, message: 'profile not found' });
+        return;
+    }
+
+    res.status(200).json({ sucess: true, updatedProfile });
+      
+    }catch(error){
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+
+  }
+
+  //delete profile by id
+  static async deleteProfile(req: Request, res: Response): Promise<void>{
+    try{
+      const profileId = parseInt(req.params.id);
+
+      // Delete the profile
+      const deletedProfile: Profile | null = await prisma.profile.delete({ where: { Id: profileId } });
+
+      if (!deletedProfile) {
+          res.status(404).json({ success: false, message: 'Profile not found' });
+          return;
+      }
+
+      res.status(200).json({ success: true, deletedProfile });
+    }catch(error){
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Internal server error' });
+    }
+  }
+  
+
 }
+
    
 
 export default ProfileController;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,14 +3,11 @@ import { Request, Response, Router } from "express";
 import userRouter from "./user.routes";
 import businessProfileRouter from "./businessProfile.routes";
 import invoiceRouter from "./invoice.routes";
-<<<<<<< HEAD
+
 import profileRouter from "./profile.routes";
-=======
 import pincodeRouter from "./pincode.routes";
 import cmsRouter from "./cms.routes";
 import postOfficeRouter from "./postOffice.routes";
->>>>>>> 2a1e88ebe1e5bd6506b75792392bb5d9e10da96a
-
 const router = Router();
 
 router.use('/user', userRouter);
@@ -18,16 +15,14 @@ router.use('/user', userRouter);
 router.use('/business', businessProfileRouter);
 
 router.use('/invoice', invoiceRouter);
-
-<<<<<<< HEAD
 router.use('/api',profileRouter)
-=======
+
 router.use('/cms', cmsRouter);
 
 router.use('/pincode', pincodeRouter);
 
 router.use('/postOffice', postOfficeRouter);
->>>>>>> 2a1e88ebe1e5bd6506b75792392bb5d9e10da96a
+
 
 router.get('/', (req, res) => {
     return res.send({ message: 'Up and running' });
