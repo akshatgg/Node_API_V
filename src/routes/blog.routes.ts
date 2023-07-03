5590a4d037cab5658c1aa114c273818183079938
theharshsingh <harshsingh.js@gmail.com>
2023-07-03 11:51:22 +0530

    Added Admin check on Blog

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import BlogController from '../controllers/blog.controller';
+import verifyToken from '../middlewares/verify-token';
+import adminCheck from '../middlewares/admin-check';
 
 const blogRouter = Router();
 
 // Create a new blog post
-blogRouter.post('/posts', BlogController.createPost);
+blogRouter.post('/posts', verifyToken, adminCheck, BlogController.createPost);
 
 // Get all blog posts
 blogRouter.get('/posts', BlogController.getAllPosts);
@@ -13,9 +15,9 @@ blogRouter.get('/posts', BlogController.getAllPosts);
 blogRouter.get('/posts/:id', BlogController.getPostById);
 
 // Update a blog post
-blogRouter.put('/posts/:id', BlogController.updatePost);
+blogRouter.put('/posts/:id', verifyToken, adminCheck, BlogController.updatePost);
 
 // Delete a blog post
-blogRouter.delete('/posts/:id', BlogController.deletePost);
+blogRouter.delete('/posts/:id', verifyToken, adminCheck, BlogController.deletePost);
 
 export default blogRouter;
