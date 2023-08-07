0b330ed6b422c4ca5a2c06a8a56ed548c24a632f
Vishwas-guptaa <vishwasgupta81779@gmail.com>
2023-08-07 16:12:26 +0530

    mca director details

--- src/controllers/sandbox/mca.controller.ts
+++ src/controllers/sandbox/mca.controller.ts
@@ -38,4 +38,39 @@ export default class MCAController {
         }
     }
 
+
+    static async getDirectorByDIN(req: Request, res: Response) {
+        try {
+            const { din } = req.query;
+
+            if(!din) {
+                return res.status(400).json({ success: false, message: 'Query parameter DIN was not provided' });
+            }
+
+            const endpoint = `${Sandbox.BASE_URL}/mca/directors/${din}`;
+
+            const token = await Sandbox.generateAccessToken();
+
+            const headers = {
+                'Authorization': token,
+                'accept': 'application/json',
+                'x-api-key': process.env.SANDBOX_KEY,
+                'x-api-version': process.env.SANDBOX_API_VERSION
+            };
+
+            const { status, data: { data } } = await axios.get(endpoint, {
+                headers
+            });
+
+            if(status !== 200) {
+                return res.status(500).send({ success: false, message: "Something went wrong" });
+            }
+            
+            return res.status(200).json({ success: true, data });
+        } catch(e) {
+            console.log(e);
+            return res.status(500).json({ status: false, message: 'Something went wrong' });
+        }
+    }
+
 }
