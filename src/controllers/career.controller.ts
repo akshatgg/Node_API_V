68ec4c76bb4feb7cd2ff42c1df72abde204a733e
Hero <76834176+jish0101@users.noreply.github.com>
2024-04-29 20:28:17 +0530

    fixed career add api | added cloudinary config

--- src/controllers/career.controller.ts
+++ src/controllers/career.controller.ts
@@ -1,145 +1,162 @@
-import { Request, Response } from 'express';
-import { Career } from '@prisma/client'
+import { Request, Response } from "express";
+import { Career } from "@prisma/client";
 import { join } from "path";
 import { cwd } from "process";
 import { prisma } from "..";
 
-
-
 const currentDir = cwd();
 
-
 export default class CareerController {
-    //create career
-    static async createCareer(req: Request, res: Response){
-        try {
-           
-            const file = req.file as Express.Multer.File; // Assuming a single CV file
+  //create career
+  static async createCareer(req: Request, res: Response) {
+    try {
+      const file = req.file as Express.Multer.File; // Assuming a single CV file
 
-            if (!file) {
-                return res.status(400).json({ success: false, message: 'No CV file to upload.' });
-            }
+      if (!file) {
+        return res
+          .status(400)
+          .json({ success: false, message: "No CV file to upload." });
+      }
 
-            const {
-                name,
-                address,
-                pin,
-                email,
-                mobile,
-                skills,
-                gender,
-            } = req.body;
+      const { name, address, pin, email, mobile, skills, gender } = req.body;
 
-            if (!name || !address || !pin || !email || !mobile || !skills || !gender) {
-                return res.status(400).json({ success: false, message: 'Missing required fields.' });
-            }
+      if (
+        !name ||
+        !address ||
+        !pin ||
+        !email ||
+        !mobile ||
+        !skills ||
+        !gender
+      ) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Missing required fields." });
+      }
 
-            const cvFileName = file.filename;
+      const { path: cvFilePath } = file;
 
-            const career = await prisma.career.create({
-                data: {
-                    cv: cvFileName, // Assuming 'cv' field corresponds to the 'fileName'
-                    name,
-                    address,
-                    pin,
-                    email,
-                    mobile,
-                    skills,
-                    gender,
-                },
-            });
+      const career = await prisma.career.create({
+        data: {
+          cv: cvFilePath, // Assuming 'cv' field corresponds to the 'fileName'
+          name,
+          address,
+          pin,
+          email,
+          mobile,
+          skills,
+          gender,
+        },
+      });
+      console.log("🚀 ~ CareerController ~ createCareer ~ career:", career);
 
-            return res.status(200).json({ success: true, message: 'Career created', career });
-        } catch (e) {
-            console.log(e);
-            return res.status(500).json({ success: false, message: 'Something went wrong' });
-        }
+      return res
+        .status(200)
+        .json({ success: true, message: "Career created", career });
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ success: false, message: "Something went wrong" });
     }
-    //getAll career
-    static async findAllCareer(req: Request, res: Response): Promise<void> {
-        try {
-            const allCareer = await prisma.career.findMany({});
+  }
+  //getAll career
+  static async findAllCareer(req: Request, res: Response): Promise<void> {
+    try {
+      const allCareer = await prisma.career.findMany({});
 
-            res.status(200).json({ success: true, allCareer });
-        } catch (error) {
-            res.status(500).json({ success: false, message: 'Internal server error', errors: error });
-        }
+      res.status(200).json({ success: true, allCareer });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        errors: error,
+      });
     }
-    //find one career
-    static async findOneCareer(req: Request, res: Response): Promise<void> {
-        try {
-           
-            const id = parseInt(req.params.id);
+  }
+  //find one career
+  static async findOneCareer(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
 
-            const Career: Career | null = await prisma.career.findUnique({
-              where: {
-                id,
-              },
-            })
+      const Career: Career | null = await prisma.career.findUnique({
+        where: {
+          id,
+        },
+      });
 
-            if (!Career) {
-                res.status(404).json({ success: false, message: 'career not found' });
-                return;
-            }
+      if (!Career) {
+        res.status(404).json({ success: false, message: "career not found" });
+        return;
+      }
 
-            res.status(200).json(Career);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: 'Internal server error' });
-        }
+      res.status(200).json(Career);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
     }
+  }
 
-    //delete career by id
-    static async deleteCareer(req: Request, res: Response): Promise<void> {
-        try {
-            const id = parseInt(req.params.id);
-            // const { id: careerId } = req.user!
-            // Delete the career
-            const deletedCareer: Career | null = await prisma.career.delete({ where: { id } });
+  //delete career by id
+  static async deleteCareer(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
+      // const { id: careerId } = req.user!
+      // Delete the career
+      const deletedCareer: Career | null = await prisma.career.delete({
+        where: { id },
+      });
 
-            if (!deletedCareer) {
-                res.status(404).json({ success: false, message: 'career not found' });
-                return;
-            }
+      if (!deletedCareer) {
+        res.status(404).json({ success: false, message: "career not found" });
+        return;
+      }
 
-            res.status(200).json({ success: true, deletedCareer });
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ success: false, message: 'Internal server error' });
-        }
+      res.status(200).json({ success: true, deletedCareer });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
     }
+  }
 
-    static async getCVByCareerId(req: Request, res: Response) {
-        try {
-            const { id } = req.params;
+  static async getCVByCareerId(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
 
-            if (!id) {
-                return res.status(400).json({ success: false, message: 'Career ID is required' });
-            }
+      if (!id) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Career ID is required" });
+      }
 
-         
+      const career = await prisma.career.findFirst({
+        where: {
+          id: parseInt(id),
+        },
+        select: {
+          cv: true,
+        },
+      });
 
-            const career = await prisma.career.findFirst({
-                where: {
-                    id:parseInt(id),
-                  
-                },
-                select: {
-                    cv: true,
-                },
-            });
+      if (!career || !career.cv) {
+        return res.status(404).json({
+          success: false,
+          message: "CV not found for the provided career ID",
+        });
+      }
 
-            if (!career || !career.cv) {
-                return res.status(404).json({ success: false, message: 'CV not found for the provided career ID' });
-            }
+      const cvFileName = career.cv;
 
-            const cvFileName = career.cv;
-
-            return res.sendFile(join(currentDir, 'uploads', cvFileName));
-        } catch (e) {
-            console.log(e);
-            return res.status(500).json({ success: false, message: 'Something went wrong' });
-        }
+      return res.sendFile(join(currentDir, "uploads", cvFileName));
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ success: false, message: "Something went wrong" });
     }
+  }
 }
-
