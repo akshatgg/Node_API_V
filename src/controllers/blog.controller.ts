fc4a42dc4e9051ad2ca3150ee14d7f468d434478
Hero <76834176+jish0101@users.noreply.github.com>
2024-05-07 00:49:12 +0530

    blog update

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,157 +1,188 @@
-import { prisma } from '../index';
-import { Request, Response } from 'express';
+import { prisma } from "../index";
+import { Request, Response } from "express";
 
 export default class BlogController {
-    
-    static async createPost(req: Request, res: Response): Promise<Response> {
-        try {
-            const { title,category, contentheading,contentdiscription } = req.body;
-            const imageUrl: string = req.file?.path as string
+  static async createPost(req: Request, res: Response): Promise<Response> {
+    try {
+      const { title, category, contentHeading, contentDescription } = req.body;
 
-            
-            if (!title || !contentheading || !imageUrl) {
-                return res.status(400).json({ success: true, message: 'Required body params are missing' });
-            }
+      const imageUrl: string = req.file?.path as string;
 
-            const user = req.user!;
+      if (!title || !contentHeading || !imageUrl) {
+        return res
+          .status(400)
+          .json({ success: true, message: "Required body params are missing" });
+      }
 
-            const post = await prisma.post.create({
-                data: {
-                    userId: user.id,
-                    title,
-                    category,
-                    contentheading,
-                    contentdiscription,
-                    imageUrl:imageUrl,
-                },
-            });
+      const user = req.user!;
 
-            return res.status(200).json({ success: true, data: post });
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({
-                success: false,
-                message: 'Something went wrong',
-            });
-        }
+      const post = await prisma.post.create({
+        data: {
+          userId: user.id,
+          title,
+          category,
+          contentHeading,
+          contentDescription,
+          imageUrl: imageUrl,
+        },
+      });
+
+      return res.status(201).json({ success: true, data: post });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
+  }
 
-    static async getAllPosts(req: Request, res: Response): Promise<Response> {
-        try {
-            // Pagination parameters
-            const { page = 1, limit = 10 } = req.query;
-            const parsedPage = parseInt(page.toString(), 10);
-            const parsedLimit = parseInt(limit.toString(), 10);
+  static async getAllPosts(req: Request, res: Response): Promise<Response> {
+    try {
+      // Pagination parameters
+      const { page = 1, limit = 10 } = req.query;
+      const parsedPage = parseInt(page.toString(), 10);
+      const parsedLimit = parseInt(limit.toString(), 10);
 
-            // Calculate the offset based on the page and limit
-            const offset = (parsedPage - 1) * parsedLimit;
-            
-            const posts = await prisma.post.findMany({
-                skip: offset,
-                take: parsedLimit,
-                orderBy: {
-                    createdAt: 'desc',
-                }
-            });
+      // Calculate the offset based on the page and limit
+      const offset = (parsedPage - 1) * parsedLimit;
 
-            return res.status(200).send({ success: true, data: { posts } });
-        } catch (error) {
-            return res.status(500).send({ success: false, message: 'Something went wrong' });
-        }
+      // Calculate the pages by limit
+      const pages = Math.ceil((await prisma.post.count()) / parsedLimit);
+
+      const posts = await prisma.post.findMany({
+        skip: offset,
+        take: parsedLimit,
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      return res.status(200).send({ success: true, data: { posts, pages } });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Something went wrong" });
     }
+  }
 
-    static async getPostById(req: Request, res: Response): Promise<Response> {
-        try {
-            const { id } = req.params;
+  static async getPostById(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
 
-            const post = await prisma.post.findUnique({ where: { id } });
+      const post = await prisma.post.findUnique({ where: { id } });
 
-            if (!post) {
-                return res.status(404).send({ success: false, message: 'Blog Post Not Found' });
-            }
+      if (!post) {
+        return res
+          .status(404)
+          .send({ success: false, message: "Blog Post Not Found" });
+      }
 
-            return res.status(200).send({ success: true, data: post });
-        } catch (error) {
-            return res.status(500).send({ success: false, message: 'Something went wrong' });
-        }
+      return res.status(200).send({ success: true, data: post });
+    } catch (error) {
+      return res
+        .status(500)
+        .send({ success: false, message: "Something went wrong" });
     }
+  }
 
-    static async updatePost(req: Request, res: Response): Promise<Response> {
-        try {
-            const { title,contentheading,category, contentdiscription, imageUrl } = req.body;
+  static async updatePost(req: Request, res: Response): Promise<Response> {
+    try {
+      const { title, contentHeading, category, contentDescription } = req.body;
+      const imageUrl: string = req.file?.path as string;
 
-            const { id } = req.params;
+      const { id } = req.params;
 
-            if (!id) {
-                return res.status(400).json({ success: true, message: 'Post ID is required for this operation' });
-            }
+      if (!id) {
+        return res.status(400).json({
+          success: true,
+          message: "Post ID is required for this operation",
+        });
+      }
 
-            if (!title || !contentheading || !imageUrl) {
-                return res.status(400).json({ success: true, message: 'Required body params are missing' });
-            }
+      if (!title || !contentHeading) {
+        return res
+          .status(400)
+          .json({ success: true, message: "Required body params are missing" });
+      }
 
-            const user = req.user!;
+      const user = req.user!;
 
-            const post = await prisma.post.findFirst({ where: { id, userId: user.id } })
+      const post = await prisma.post.findFirst({
+        where: { id, userId: user.id },
+      });
 
-            if (!post) {
-                return res.status(404).json({ success: true, message: 'Post not found' });
-            }
+      if (!post) {
+        return res
+          .status(404)
+          .json({ success: true, message: "Post not found" });
+      }
 
-            if (post.userId !== user.id) {
-                return res.status(403).json({ success: true, message: 'Unauthorized Access' });
-            }
+      if (post.userId !== user.id) {
+        return res
+          .status(403)
+          .json({ success: true, message: "Unauthorized Access" });
+      }
 
-            const updatedPost = await prisma.post.update({
-                where: { id },
-                data: {
-                    title,
-                    category,
-                    contentheading,
-                    contentdiscription,
-                    imageUrl,
-                },
-            });
+      const updatedPost = await prisma.post.update({
+        where: { id },
+        data: {
+          title,
+          category,
+          contentHeading,
+          contentDescription,
+          imageUrl: imageUrl ?? post.imageUrl,
+        },
+      });
 
-            return res.status(200).json({ success: true, data: updatedPost });
-        } catch (error) {
-            return res.status(500).json({
-                success: false,
-                message: 'Something went wrong',
-            });
-        }
+      return res.status(200).json({ success: true, data: updatedPost });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
+  }
 
-    static async deletePost(req: Request, res: Response): Promise<Response> {
-        try {
-            const { id } = req.params;
+  static async deletePost(req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params;
 
-            if (!id) {
-                return res.status(400).json({ success: true, message: 'Post ID is required for this operation' });
-            }
+      if (!id) {
+        return res.status(400).json({
+          success: true,
+          message: "Post ID is required for this operation",
+        });
+      }
 
-            const user = req.user!;
+      const user = req.user!;
 
-            const post = await prisma.post.findFirst({ where: { id, userId: user.id } })
+      const post = await prisma.post.findFirst({
+        where: { id, userId: user.id },
+      });
 
-            if (!post) {
-                return res.status(404).json({ success: true, message: 'Post not found' });
-            }
+      if (!post) {
+        return res
+          .status(404)
+          .json({ success: true, message: "Post not found" });
+      }
 
-            if (post.userId !== user.id) {
-                return res.status(403).json({ success: true, message: 'Unauthorized Access' });
-            }
+      if (post.userId !== user.id) {
+        return res
+          .status(403)
+          .json({ success: true, message: "Unauthorized Access" });
+      }
 
-            await prisma.post.delete({
-                where: { id },
-            });
+      await prisma.post.delete({
+        where: { id },
+      });
 
-            return res.status(200).json({ success: true, message: 'Post deleted' });
-        } catch (error) {
-            return res.status(500).json({
-                success: false,
-                message: 'Something went wrong',
-            });
-        }
+      return res.status(200).json({ success: true, message: "Post deleted" });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
+  }
 }
