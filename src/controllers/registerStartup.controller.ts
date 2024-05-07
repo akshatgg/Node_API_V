6219d4ce444f97bb31ea5b3e98819b5b6bf03fd6
Hero <76834176+jish0101@users.noreply.github.com>
2024-05-07 21:00:42 +0530

    update

--- src/controllers/registerStartup.controller.ts
+++ src/controllers/registerStartup.controller.ts
@@ -1,86 +1,182 @@
-import { Request, Response } from 'express';
-import { prisma } from '../index';
-import { RegisterStartup } from '@prisma/client'
+import { Request, Response } from "express";
+import { prisma } from "../index";
+import { RegisterStartup } from "@prisma/client";
+
 export class RegisterStartupController {
+  static async RegisterStartup(req: Request, res: Response) {
+    try {
+      const image: string = req.file?.path as string;
+      const { title, categories } = req.body;
+      const { id: userId } = req.user!;
 
-    static async RegisterStartup(req: Request, res: Response) {
-        try {
-            const image: string = req.file?.path as string
-            const { title, categories } = req.body
-            const { id: userId } = req.user!;
+      if (!title) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query title name was not provided",
+        });
+      }
+      if (!image) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query image was not provided",
+        });
+      }
+      if (!categories) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query categories was not provided",
+        });
+      }
 
-            if (!title) {
-                return res.status(400).json({ success: false, message: 'Required Query title name was not provided' });
-            }
-            if (!image) {
-                return res.status(400).json({ success: false, message: 'Required Query image was not provided' });
+      const existingUser = await prisma.user.findUnique({
+        where: { id: userId },
+      });
 
-            }
-            if (!categories) {
-                return res.status(400).json({ success: false, message: 'Required Query categories was not provided' });
+      if (!existingUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
 
-            }
+      const newRegisterStarup: RegisterStartup =
+        await prisma.registerStartup.create({
+          data: {
+            title,
+            image,
+            userId,
+            categories,
+          },
+        });
 
-            const existingUser = await prisma.user.findUnique({ where: { id: userId } });
-            if (!existingUser) {
-                return res.status(404).json({ error: 'User not found' });
-
-            }
-            const newRegisterStarup: RegisterStartup = await prisma.registerStartup.create({
-                data: {
-                    title, image, userId, categories
-                }
-            })
-            return res.status(201).json({ result: newRegisterStarup, message: 'Sucessfull Register Startup Setting created' });
-        } catch (error) {
-
-            return res.status(500).json({ success: false, message: 'Internal server error', errors: error });
-        }
+      return res.status(201).json({
+        result: newRegisterStarup,
+        message: "Sucessfully Register Startup Setting created",
+      });
+    } catch (error) {
+      console.log(
+        "🚀 ~ RegisterStartupController ~ RegisterStartup ~ error:",
+        error
+      );
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        errors: error,
+      });
     }
+  }
 
-    static async findAllStartup(req: Request, res: Response) {
-        try {
-            const AllStartup = await prisma.registerStartup.findMany({});
+  static async findAllStartup(req: Request, res: Response) {
+    try {
+      const AllStartup = await prisma.registerStartup.findMany({});
 
-            return res.status(200).json({ success: true, AllStartup });
-        } catch (error) {
-            return res.status(500).json({ success: false, message: 'Internal server error', errors: error });
-        }
+      return res.status(200).json({ success: true, AllStartup });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+        errors: error,
+      });
     }
+  }
 
-    static async getById(req: Request, res: Response) {
-        try {
+  static async getById(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
 
-            const { id } = req.params;
+      // Get the invoice by ID
+      const Startup: RegisterStartup | null =
+        await prisma.registerStartup.findUnique({
+          where: { id: parseInt(id) },
+        });
 
-            // Get the invoice by ID
-            const Startup: RegisterStartup | null = await prisma.registerStartup.findUnique({
-                where: { id: parseInt(id) },
+      if (!Startup) {
+        return res
+          .status(404)
+          .json({ sucess: false, message: "Startup not found" });
+      }
 
-            });
-
-            if (!Startup) {
-                return res.status(404).json({ sucess: false, message: 'Startup not found' });
-
-            }
-
-            return res.status(200).json(Startup);
-        } catch (error) {
-            return res.status(500).json({ sucess: false, message: 'Internal server error' });
-        }
+      return res.status(200).json(Startup);
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ sucess: false, message: "Internal server error" });
     }
+  }
 
-    static async delete(req: Request, res: Response) {
-        try {
-            const { id } = req.params;
-            const deletedStartup = await prisma.registerStartup.delete({
-                where: { id: parseInt(id) },
+  static async delete(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
 
-            });
-            return res.status(200).json({ success: true, deletedStartup, message: "Register Startup deleted sucessfully" });
-        } catch (error) {
-            return res.status(500).json({ success: false, message: 'Internal server error' });
-        }
+      const deletedStartup = await prisma.registerStartup.delete({
+        where: { id: parseInt(id) },
+      });
+
+      return res.status(200).json({
+        success: true,
+        deletedStartup,
+        message: "Register Startup deleted sucessfully",
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
     }
+  }
 
+  static async update(req: Request, res: Response) {
+    try {
+      const { id, title, categories } = req.body;
+      const { id: userId } = req.user!;
+      const image: string = req.file?.path as string;
+
+      if (!title) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query title name was not provided",
+        });
+      }
+      if (!image) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query image was not provided",
+        });
+      }
+      if (!categories) {
+        return res.status(400).json({
+          success: false,
+          message: "Required Query categories was not provided",
+        });
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: { id: userId },
+      });
+
+      if (!existingUser) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      const existingStartup = await prisma.registerStartup.findFirst({
+        where: { id: parseInt(id) },
+      });
+
+      const updatedStartup = await prisma.registerStartup.update({
+        where: { id: parseInt(id) },
+        data: {
+          title,
+          categories,
+          userId,
+          image: image ?? existingStartup?.image,
+        },
+      });
+
+      return res.status(200).json({
+        success: true,
+        updatedStartup,
+        message: "Register Startup deleted sucessfully",
+      });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ success: false, message: "Internal server error" });
+    }
+  }
 }
--- src/routes/registerStartup,routes.ts
+++ src/routes/registerStartup,routes.ts
@@ -1,14 +1,36 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/verify-token";
 import { RegisterStartupController } from "../controllers/registerStartup.controller";
-import { upload } from "../config/file-upload";
- const registerStartupRouter = Router()
+import { multerInstance } from "../config/cloudinaryUploader";
 
+const registerStartupRouter = Router();
 
+registerStartupRouter.post(
+  "/register",
+  verifyToken,
+  multerInstance.single("image"),
+  RegisterStartupController.RegisterStartup
+);
+registerStartupRouter.get(
+  "/getAll",
+  verifyToken,
+  RegisterStartupController.findAllStartup
+);
+registerStartupRouter.put(
+  "/register/:id",
+  verifyToken,
+  multerInstance.single("image"),
+  RegisterStartupController.update
+);
+registerStartupRouter.get(
+  "/getOne/:id",
+  verifyToken,
+  RegisterStartupController.getById
+);
+registerStartupRouter.delete(
+  "/delete/:id",
+  verifyToken,
+  RegisterStartupController.delete
+);
 
-registerStartupRouter.post('/register',upload.single('image'), verifyToken, RegisterStartupController.RegisterStartup);
-registerStartupRouter.get('/getAll', verifyToken, RegisterStartupController.findAllStartup);
-registerStartupRouter.get('/getOne/:id', verifyToken, RegisterStartupController.getById);
-registerStartupRouter.delete('/delete/:id', verifyToken, RegisterStartupController.delete);
-
-export default registerStartupRouter
+export default registerStartupRouter;
